0a462167ba2586020cbcee893a0bd1d777264ec5
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2022-05-12 23:17:48 +0300

    add config.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+// O5cHJFYflk1ZANAx
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const { DB_HOST } = require("./config");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect"))
+  .catch((error) => console.log(error.message));
+
+// const app = require("./app");
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
