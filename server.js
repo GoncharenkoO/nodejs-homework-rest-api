6db298ee64144aa63303a8111153e18f45714794
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2022-05-13 21:02:35 +0300

    hw03-mongoodb

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 
 const mongoose = require("mongoose");
 
-require("dotenv").config();
+const app = require("./app");
 
 const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connect"))
-  .catch((error) => console.log(error.message));
-
-// const app = require("./app");
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
