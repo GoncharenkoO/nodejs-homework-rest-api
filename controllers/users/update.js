b1d3cdc5f19277d65d0a004cb4765627801c8d12
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2022-05-27 19:30:23 +0300

    hw05-awatars

--- controllers/users/update.js
+++ controllers/users/update.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 const { User } = require("../../models/User");
 const { createError } = require("../../helpers");
 
@@ -12,6 +13,8 @@ const update = async (req, res, next) => {
     const [extension] = filename.split(".").reverse();
     const name = `${_id}.${extension}`;
     const resultDir = path.join(avatarsDir, name);
+    const image = await Jimp.read(resultDir);
+    await image.resize(250, 250).write(resultDir);
     await fs.rename(tempDir, resultDir);
     const avatarURL = path.join("avatars", name);
     await User.findByIdAndUpdate(_id, { avatarURL });
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,5 @@
 const multer = require("multer");
 const path = require("path");
-// const Jimp = require("jimp");
 
 const tempDir = path.join(__dirname, "../", "temp");
 
@@ -17,16 +16,5 @@ const multerConfig = multer.diskStorage({
 const upload = multer({
   storage: multerConfig,
 });
-
-// Jimp.read("./avatar.jpg")
-//   .then((avatar) => {
-//     return avatar
-//       .resize(250, 250)
-//       .quality(60)
-//       .write(__dirname + "avatar-bw.jpg");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
 
 module.exports = upload;
